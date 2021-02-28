b396eda84c3b5778e2122f15c1430dbdfe3baea9
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-28 12:59:48 -0500

    Create 266A.java

---
+++ 266A.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        nextInt();
+        String str = next();
+        char cur = str.charAt(0);
+        int ans = 0;
+        for (int i = 1; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (cur == c) {
+                ans++;
+            } else {
+                cur = c;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}

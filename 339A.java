2c23c92ad6991c5b4bf0fa7511ff0c21c5ea140d
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-28 12:40:47 -0500

    Create 339A.java

---
+++ 339A.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        String[] nums = next().split("\\+");
+        Arrays.sort(nums);
+        System.out.print(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            System.out.print("+" + nums[i]);
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}

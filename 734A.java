3e9b5a50f7f514c23ea40a7a4c45dcb2ac06087b
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-28 15:11:47 -0500

    Create 734A.java

---
+++ 734A.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        nextInt();
+        String games = next();
+        int anton = games.replaceAll("D", "").length();
+        int danik = games.replaceAll("A", "").length();
+        if (anton > danik) {
+            System.out.println("Anton");
+        } else if (danik > anton) {
+            System.out.println("Danik");
+        } else {
+            System.out.println("Friendship");
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}

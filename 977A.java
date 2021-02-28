f2fc6488ed88587feedc6c8d578dd268136eff2f
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-28 13:41:31 -0500

    Create 977A.java

---
+++ 977A.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt();
+        int k = nextInt();
+        for (int i = 0; i < k; i++) {
+            if (String.valueOf(n).endsWith("0")) {
+                n /= 10;
+            } else {
+                n--;
+            }
+        }
+        System.out.println(n);
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}

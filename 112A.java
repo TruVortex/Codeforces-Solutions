b36a836bea2149b752a5133a928a4ca7b875a827
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-28 11:29:08 -0500

    Create 112A.java

---
+++ 112A.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        String str = next().toLowerCase();
+        String[] strings = {str, next().toLowerCase()};
+        Arrays.sort(strings);
+        if (strings[0].equals(strings[1])) {
+            System.out.println(0);
+        } else if (str.equals(strings[0])) {
+            System.out.println(-1);
+        } else {
+            System.out.println(1);
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}

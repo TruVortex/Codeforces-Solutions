580df67e7fcff60ffb28001fe030bdd8a4a68967
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-28 12:55:12 -0500

    Create 281A.java

---
+++ 281A.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        String str = next();
+        System.out.println(String.valueOf(str.charAt(0)).toUpperCase() + str.substring(1));
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
